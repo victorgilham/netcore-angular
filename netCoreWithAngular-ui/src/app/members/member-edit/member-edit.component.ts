064a018296a23979c0792ef813cb543ca6836969
victorgilham <gilhamhugo@hotmail.com>
2019-05-23 23:32:49 -0500

    added edit user info component

--- netCoreWithAngular-ui/src/app/members/member-edit/member-edit.component.ts
+++ netCoreWithAngular-ui/src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,8 @@
+import { AlertService } from './../../_services/alert.service';
 import { ActivatedRoute } from '@angular/router';
 import { User } from './../../_models/user';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-member-edit',
@@ -8,14 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editUserInfoForm') editUserInfoForm: NgForm;
   user: User;
   data: string;
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private alertService: AlertService
+  ) {}
 
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.user = data.user;
     });
-    this.data = ' Data from component';
   }
+  updateUser = () => {
+    console.log(this.user);
+    this.alertService.success('Info updated');
+    this.editUserInfoForm.reset(this.user);
+  };
 }
